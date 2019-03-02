c6b79a06c81c3eee79f4f7aecbd61da6a3a86a26
Kaushik <Kaushik@Kaushiks-MBP.fios-router.home>
2019-03-02 18:29:34 -0500

    So i can work on mateusz's pc

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import numpy as np
 
+df = pd.read_csv("Datasets/NSDUH_2017.csv")
+ages = df.to_numpy()
 
+#2505
+print(ages.shape[0])
+
+for i in range(ages.shape[0]):
+    age = ages[i][2504]
+    num = age[:2]
+    if int(num) < 17 or int(num) > 19:
+        np.delete(ages, i, axis=0)
+
+df.to_csv("Datasets/NSDUH_2017.csv")
 
 
