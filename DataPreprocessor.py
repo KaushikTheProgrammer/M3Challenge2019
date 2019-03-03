b97f1581a27c9e6f0988a51c8994886e12aa981a
mwolak07 <mwolak07@gmail.com>
2019-03-02 22:00:10 -0500

    My stuff

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("Datasets/NSDUH_2017.csv")
-ages = df.to_numpy()
+df = pd.read_csv("Datasets/NSDUH_MODIFIED_2017.csv")
 
-#2505
-print(ages.shape[0])
 
-for i in range(ages.shape[0]):
-    age = ages[i][2504]
-    num = age[:2]
-    if int(num) < 17 or int(num) > 19:
-        np.delete(ages, i, axis=0)
 
-df.to_csv("Datasets/NSDUH_2017.csv")
-
+# ages = df.to_numpy()
+#
+# np.random.shuffle(ages)
+# #2505
+# print("starting loop")
+#
+# new_ages = np.copy(ages[:3000])
+#
+# for i in range(new_ages.shape[0]):
+#     print(new_ages[i])
+#     age = new_ages[i][2504]
+#     num = age[:2]
+#     if int(num) < 17 or int(num) > 19:
+#         np.delete(new_ages, i, axis=0)
+# df = pd.DataFrame(new_ages)
+# df.to_csv("Datasets/NSDUH_MODIFIED_2017.csv")
 
